f6ed7043afc139fcf3c487d0c938627eca683ceb
jvrsgsty <jvrsgsty@users.noreply.github.com>
2020-10-01 06:19:18 +0000

    Save to gs bucket

--- train_tpu.sh
+++ train_tpu.sh
@@ -12,6 +12,6 @@ export TPU_IP_ADDRESS=10.144.244.18
 #export TPU_IP_ADDRESS=10.140.146.146
 export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
 
-python train.py --overwrite --experiment=tpu-test --expid=test2 --verbose --save-dir=/home/jvrsgsty/Results --data-dir=/home/jvrsgsty/data --model=fc --train-batch-size=256 --epochs=10 --save-freq=20 --tpu=True
+python train.py --overwrite --experiment=mnist --expid=tpu --verbose --save-dir=gs://jvr-pt-tpu --lr=0.01 --data-dir=/home/jvrsgsty/data --model=fc --train-batch-size=32 --epochs=10 --save-freq=20 --tpu=True
 
 
