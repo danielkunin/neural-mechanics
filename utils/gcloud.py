df13d6402677ec1cf517a9b777aab3cee00d92c1
jvrsgsty <jvrsgsty@users.noreply.github.com>
2020-09-30 19:42:29 -0700

    Glob based approach to copy checkpoints over to gcloud

--- utils/gcloud.py
+++ utils/gcloud.py
@@ -1,4 +1,5 @@
 import os
+import glob
 from google.cloud import storage
 
 
@@ -7,9 +8,12 @@ def post_file_to_bucket(filename):
     assert filename[0:5] == "gs://"
     gcs = storage.Client()
     bucket = gcs.get_bucket(filename.split("gs://")[1].split("/")[0])
-    remote_filename = "/".join(filename.split("gs://")[1].split("/")[1:])
-    blob = bucket.blob(remote_filename)
-    blob.upload_from_filename(filename=filename)
-    print_fn(f"File {filename} posted to gcs")
-    # TODO: check for success?
-    os.remove(filename)
+    glob_path = f"{filename.split('step')[0]}step*.{filename.split('.')[1]}"
+    paths = glob.glob(glob_path)
+    for file in paths:
+        remote_filename = "/".join(file.split("gs://")[1].split("/")[1:])
+        blob = bucket.blob(remote_filename)
+        blob.upload_from_filename(filename=file)
+        print_fn(f"File {file} posted to gcs")
+        # TODO: check for success?
+        os.remove(file)
