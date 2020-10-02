b666125962a5f35f30d1c6bdb7012a2add8a3f6f
jvrsgsty <jvrsgsty@users.noreply.github.com>
2020-10-02 23:04:19 +0000

    Removes the verbose flag

--- scripts/vgg16_train_tpu_08.sh
+++ scripts/vgg16_train_tpu_08.sh
@@ -5,4 +5,4 @@
 export TPU_IP_ADDRESS=10.8.170.2
 export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
 
-python train.py --overwrite --experiment=tinyimagenet --expid=vgg16-bn_bs256_lr1en1_wd0 --verbose --save-dir=gs://jvr-pt-tpu --dataset=tiny-imagenet --data-dir=/home/jvrsgsty/data --model=vgg16-bn --model-class=tinyimagenet --train-batch-size=32 --lr=0.1 --wd=0 --optimizer=custom_sgd --epochs=100 --save-freq=500 --tpu=True --workers=8
+python train.py --overwrite --experiment=tinyimagenet --expid=vgg16-bn_bs256_lr1en1_wd0  --save-dir=gs://jvr-pt-tpu --dataset=tiny-imagenet --data-dir=/home/jvrsgsty/data --model=vgg16-bn --model-class=tinyimagenet --train-batch-size=32 --lr=0.1 --wd=0 --optimizer=custom_sgd --epochs=100 --save-freq=500 --tpu=True --workers=8
